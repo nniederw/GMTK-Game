44e597bcf0798c24a201ef303f13831b6eb7546a
SaSt8903 <shadowdeath@gmx.net>
2020-07-11 00:55:05 +0200

    update movement

--- GMTK-Game/Assets/Scripts/PlayerMovement.cs
+++ GMTK-Game/Assets/Scripts/PlayerMovement.cs
@@ -6,9 +6,17 @@ public class PlayerMovement : MonoBehaviour
 {
     public float MoveSpeed = 5f;
     public Rigidbody2D rb;
+    Vector2 movement;
 
     void Update()
-    {
-        
+    {//Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
     }
+
+    void FixedUpdate()
+    {//Movement
+        rb.MovePosition(rb.position + movement * MoveSpeed * Time.fixedDeltaTime);
+    }
+
 }
