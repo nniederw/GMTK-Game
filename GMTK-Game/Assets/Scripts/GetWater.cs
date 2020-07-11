927b66b66155a50615aeec7acf4ac045dd2657b9
nn <niels.niederwieser@gmx.ch>
2020-07-11 04:04:12 +0200

    u

--- GMTK-Game/Assets/Scripts/GetWater.cs
+++ GMTK-Game/Assets/Scripts/GetWater.cs
@@ -6,16 +6,44 @@ public class GetWater : MonoBehaviour
 {
 
     [SerializeField] GameObject Player;
-
-    public void OnTriggerStay2D(Collider2D collision)
-    {   Debug.Log("hallo");
-        if (collision.gameObject == Player)
+    private List<GameObject> gObjects = new List<GameObject>();
+    void Update()
+    {
+        gObjects.ForEach(OnCollision);
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        gObjects.Add(col.gameObject);
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        gObjects.Remove(col.gameObject);
+    }
+    void OnCollision(GameObject gameObject)
+    {
+        if (gameObject == Player)
         {
-            if (Input.GetKey("e"))
+            if (Input.GetKeyDown("e"))
             {
+                Debug.Log("e");
                 Player.GetComponent<PlayerVariables>().Water = true;
             }
         }
     }
+    /*
+    public void OnTriggerStay2D(Collider2D collision)
+    {
 
+        Debug.Log("called");
+        if (collision.gameObject == Player)
+        {
+            Debug.Log("gameobject found");
+            if (Input.GetKey("e"))
+            {
+                Debug.Log("e");
+                Player.GetComponent<PlayerVariables>().Water = true;
+            }
+        }
+    }
+    */
 }
