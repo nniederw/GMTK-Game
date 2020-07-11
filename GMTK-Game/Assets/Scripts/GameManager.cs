779a6c08abfc4c409667c169f19f2216cd1a701d
SaSt8903 <shadowdeath@gmx.net>
2020-07-11 03:19:54 +0200

    managing

---
+++ GMTK-Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager
+{
+    public static void SpreadFire()
+    {
+        //todo 
+        Debug.Log("this girl is on fire");
+    }
+}
