8c113c966b9bd7448b5f3b15bf7e5804fda17d7f
nn <niels.niederwieser@gmx.ch>
2020-07-12 13:42:16 +0200

    u

--- GMTK-Game/Assets/Scripts/GetWater.cs
+++ GMTK-Game/Assets/Scripts/GetWater.cs
@@ -4,11 +4,7 @@ using UnityEngine;
 
 public class GetWater : MonoBehaviour
 {
-<<<<<<< HEAD
-    [SerializeField] PlayerVariables playerVariables;
-=======
     [SerializeField] PlayerVariables playerVariables = null;
->>>>>>> f5492d87c1902104657671461d045f38ceb5b24c
     private List<GameObject> gObjects = new List<GameObject>();
     void Update()
     {
@@ -28,8 +24,6 @@ public class GetWater : MonoBehaviour
         {
             if (Input.GetKeyDown("e"))
             {
-<<<<<<< HEAD
-                Debug.Log("e");
                 playerVariables.Water = true;
             }
         }
@@ -50,10 +44,4 @@ public class GetWater : MonoBehaviour
         }
     }
     */
-=======
-                playerVariables.Water = true;
-            }
-        }
-    }    
->>>>>>> f5492d87c1902104657671461d045f38ceb5b24c
 }
